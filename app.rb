ded319dcd2ef51f139166c2fe7d8b12074d6aea5
Mike McTighe <m.mctighe10@gmail.com>
2019-10-18 02:50:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+
+  get '/name' do
+    "My name is Mike"
+  end
+
+  get "/hometown" do
+  "My hometown is RVC"
+end
+
+  get "/favorite-song" do
+  "My favorite song is The Kill by Napalm death"
+end
 end
