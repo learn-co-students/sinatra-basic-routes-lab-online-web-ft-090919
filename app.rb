21280e39bfe00e518a14ad3226975d998cf379b5
mpuchyr <matt.puchyr@gmail.com>
2019-10-18 15:43:07 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Matt."
+    end
+
+    get '/hometown' do
+        "My hometown is Elgin."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is What a Shame by Shinedown."
+    end
 end
