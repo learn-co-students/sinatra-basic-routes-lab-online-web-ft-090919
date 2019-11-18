71783bb7ce22c3380c31abd7c482ad00cc88adcf
Shaqqour M <mshaqqourm@gmail.com>
2019-11-18 02:37:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Shaqqour"
+    end
+
+    get '/hometown' do
+        "My hometown is Nablus/Palestine"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Ya Zareef El Tool"
+    end
+
 end
